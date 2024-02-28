f2a799867e5c77860bf40ab27af235348414d94a
Himanshi Aggarwal <85385551+Himanshi-200@users.noreply.github.com>
2024-02-28 20:17:58 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+function Create() {
+	alert("New Record Created Successfully!");
+}
+
+function Read() {
+	alert("All Records!");
+}
+
+
+function Update() {
+	alert("Record is Updated Successfully!");
+}
+ 
+function Delete() {
+	alert("Are you sure you want to delete this record?");
+}
+
+function Logout() {
+	alert("Are you sure you want to log out?");
+}
+
+function OpenMainPage() {
+	window.location.href = "MainPage.html";
+}
